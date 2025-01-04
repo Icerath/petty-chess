06218c9eff7e7552b7412a0a7651c39dce69e19c
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-01-04 12:07:45 +0200

    oops

--- src/main.rs
+++ src/main.rs
@@ -223,6 +223,5 @@ fn perft(board: &mut Board, depth: u8) -> u64 {
         board.unmake_move(unmake);
         eprintln!("{mov}: {count}");
     }
-    println!("{}", table.capacity());
     total
 }
