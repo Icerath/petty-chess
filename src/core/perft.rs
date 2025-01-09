b3fc5025e9d69b8e6b9a57f81db16fae546da1fd
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-01-09 12:57:38 +0200

    setup tests for miri

--- src/core/perft.rs
+++ src/core/perft.rs
@@ -36,7 +36,11 @@ pub(crate) mod tests {
 
     #[test]
     fn perft_start() {
+        #[cfg(miri)]
+        let results = [1, 20, 400];
+        #[cfg(not(miri))]
         let results = [1, 20, 400, 8_902, 197_281, 4_865_609, 119_060_324];
+
         for (depth, &result) in results.iter().enumerate() {
             let count = Board::start_pos().run_perft(depth as u8);
             assert_eq!(count, result, "depth: {depth}");
@@ -45,6 +49,9 @@ pub(crate) mod tests {
 
     #[test]
     pub(crate) fn perft_kiwi() {
+        #[cfg(miri)]
+        let results = [1, 48];
+        #[cfg(not(miri))]
         let results = [1, 48, 2_039, 97_862, 4_085_603, 193_690_690];
         for (depth, &result) in results.iter().enumerate() {
             let count = Board::kiwipete().run_perft(depth as u8);
@@ -53,6 +60,9 @@ pub(crate) mod tests {
     }
     #[test]
     fn perft_position_3() {
+        #[cfg(miri)]
+        let results = [1, 14, 191];
+        #[cfg(not(miri))]
         let results = [1, 14, 191, 2_812, 43_238, 674_624, 11_030_083, 178_633_661];
         for (depth, &result) in results.iter().enumerate() {
             let count = Board::perft_position_3().run_perft(depth as u8);
@@ -61,6 +71,9 @@ pub(crate) mod tests {
     }
     #[test]
     fn perft_position_4() {
+        #[cfg(miri)]
+        let results = [1, 6, 264];
+        #[cfg(not(miri))]
         let results = [1, 6, 264, 9_467, 422_333, 15_833_292 /*, 706_045_033*/];
         for (depth, &result) in results.iter().enumerate() {
             let count = Board::perft_position_4().run_perft(depth as u8);
@@ -69,6 +82,9 @@ pub(crate) mod tests {
     }
     #[test]
     fn perft_talk() {
+        #[cfg(miri)]
+        let results = [1, 44];
+        #[cfg(not(miri))]
         let results = [1, 44, 1_486, 62_379, 2_103_487, 89_941_194];
         for (depth, &result) in results.iter().enumerate() {
             let count = Board::perft_position_5().run_perft(depth as u8);
