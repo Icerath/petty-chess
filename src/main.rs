f9aa6b6e29da885eb7060af536fe8ff2aff6aa09
Dorje Gilfillan <dorjegilfillan@gmail.com>
2025-01-10 11:50:13 +0200

    don't spawn kill thread for infinite searches

--- src/main.rs
+++ src/main.rs
@@ -146,10 +146,12 @@ impl Application {
             tracing::info!("Num transpositions: {}", engine.transposition_table.num_hits);
         });
         let kill = self.kill.clone();
-        std::thread::spawn(move || {
-            std::thread::sleep(time_available);
-            kill.store(true, Ordering::Relaxed);
-        });
+        if time_available != Duration::MAX {
+            std::thread::spawn(move || {
+                std::thread::sleep(time_available);
+                kill.store(true, Ordering::Relaxed);
+            });
+        }
     }
 
     fn go_perft(&mut self, depth: u8) {
