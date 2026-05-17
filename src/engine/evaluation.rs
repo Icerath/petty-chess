32f03e1cd581ca6dfacabaae285c6ac949331aac
Icerath <dorjegilfillan@gmail.com>
2026-05-17 21:17:46 +0200

    simplify king safety

--- src/engine/evaluation.rs
+++ src/engine/evaluation.rs
@@ -90,25 +90,17 @@ impl<'a, F: FnMut(&'static str, Side, i32)> Evaluation<'a, F> {
     }
 
     fn king_safety(&mut self, side: Side) {
-        self.total[side] -=
-            (self.board.get(!side + Queen) & self.king[side].nearby3()).count() as i32 * 30;
-        self.total[side] -= (self.board.get(!side + Knight)
-            | self.board.get(!side + Bishop) & self.king[side].nearby3())
-        .count() as i32
-            * 15;
-        self.total[side] -=
-            (self.board.get(!side + Rook) & self.king[side].nearby3()).count() as i32 * 20;
+        let nearby3 = self.king[side].nearby3();
+        let nearby2 = self.king[side].nearby2();
 
-        self.total[side] -=
-            (self.board.get(!side + Queen) & self.king[side].nearby2()).count() as i32 * 30;
-        self.total[side] -= (self.board.get(!side + Knight)
-            | self.board.get(!side + Bishop) & self.king[side].nearby2())
-        .count() as i32
-            * 15;
-        self.total[side] -= (self.board.get(!side + Rook)
-            | self.board.get(!side + Rook) & self.king[side].nearby3())
-        .count() as i32
-            * 20;
+        self.total[side] -= (self.board.get(!side + Bishop) & nearby2).count() as i32 * 15;
+        self.total[side] -= (self.board.get(!side + Knight) & nearby2).count() as i32 * 15;
+        self.total[side] -= (self.board.get(!side + Queen) & nearby2).count() as i32 * 30;
+
+        self.total[side] -= (self.board.get(!side + Bishop) & nearby3).count() as i32 * 15;
+        self.total[side] -= (self.board.get(!side + Knight) & nearby3).count() as i32 * 15;
+        self.total[side] -= (self.board.get(!side + Rook) & nearby3).count() as i32 * 40;
+        self.total[side] -= (self.board.get(!side + Queen) & nearby3).count() as i32 * 30;
 
         self.total[side] -=
             i32::from(queen_attacks(self.king[side], self.occupancy & !self.board[side]).count())
