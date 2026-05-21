0b5927e457590ebc30c7ae77c4acdd6f5bdd6d9f
Icerath <dorjegilfillan@gmail.com>
2026-05-21 21:43:39 +0200

    fix 50 move rule (I think)

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -71,7 +71,8 @@ impl Engine {
         if self.depth_from_root != 0 && self.seen_position() {
             return Ok(if self.depth_from_root.is_multiple_of(2) { -Score(20) } else { Score(20) });
         }
-        if self.depth_from_root != 0 && self.board.halfmove_clock >= 50 {
+        // FIXME: confirm that > 100 is correct
+        if self.depth_from_root != 0 && self.board.halfmove_clock > 100 {
             return Ok(Score(0));
         }
 
